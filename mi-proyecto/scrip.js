41bc5c5e3722d2caa34d4af602d7d2cbb5744247
juandy412 <jdgarciaq@ufpso.edu.co>
2026-04-08 20:21:28 -0600

    paginacon elementos de blotstrap

--- mi-proyecto/scrip.js
+++
@@ -1,9 +0,0 @@
-const btn = document.getElementById("themeBtn");
-btn.addEventListener("click", () => {
-document.body.classList.toggle("dark");
-if (document.body.classList.contains("dark")) {
-btn.textContent ="Modo claro";
-} else {
-btn.textContent ="Modo oscuro";
-}
-});
